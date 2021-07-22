dc0f92a5549d5b5154b254b0290819954a8b8d21
ives costa <ivescosta@MacBook-Air-de-ives.local>
2021-07-22 12:07:48 -0500

    ëxemploo 2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LearningApp.Model;
+
+namespace LearningApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1;
+
+            produto1 = new Produto("TV", 4.500, 40 );
+            produto1.RemoverItemEstoque(5);
+            Console.WriteLine(produto1);
+            produto1.AdicionarItemEstoque(15);
+            Console.WriteLine(produto1);
+            
+        }
+    }
+}
