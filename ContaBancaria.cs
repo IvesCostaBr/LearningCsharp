da8b3a2aadc691819074567c7e445c0be68792ae
ives costa <ivescosta@MacBook-Air-de-ives.local>
2021-07-23 08:46:48 -0500

    solved problematic ContaBancaria

---
+++ ContaBancaria.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace LearningApp.Conta
+{
+    public class ContaBancaria
+    {
+        public string Titular { get; private set; }
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; private set; }
+        const double taxaSaque = 5.00;
+
+
+
+        public ContaBancaria(string titular ,int numero) { //Sobrecarga de construtores
+            NumeroConta = numero;
+            Titular = titular;
+
+        }
+
+        public ContaBancaria( string titular, int numero, double saldo) :this(titular, numero) {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valorSaque) {
+            if (valorSaque <= Saldo)
+            {
+                Console.WriteLine($"Saldo anterior: {Saldo}");
+                Saldo -= valorSaque - taxaSaque;
+                Console.WriteLine($"Saque realizado, Novo saldo{Saldo}");
+            }
+            else {
+                Console.WriteLine("Impossivel realizar o saque!Sem saldo suficiente.");
+            }
+
+        }
+
+        public void Deposito(double valorDeposito) { 
+            Saldo += valorDeposito;
+            Console.WriteLine(this);
+        }
+
+        public override string ToString() //__str__
+        {
+            return " Conta :"
+                + NumeroConta
+                + ", Titular:"
+                + Titular
+                + ", Saldo: R$  "
+                + Saldo;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,39 @@
 ﻿using System;
-using LearningApp.Model;
+using LearningApp.Conta;
 
 namespace LearningApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Produto produto1;
+            ContaBancaria conta;
 
-            produto1 = new Produto("TV", 4.500, 40 );
-            produto1.RemoverItemEstoque(5);
-            Console.WriteLine(produto1);
-            produto1.AdicionarItemEstoque(15);
-            Console.WriteLine(produto1);
-            
+            Console.WriteLine("Digite o nome do titular:");
+            string nomeTitular = Console.ReadLine();
+            Console.WriteLine("Digite o numero da conta:");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Avera deposito inicial");
+            string resp = Console.ReadLine();
+
+
+            if (resp == "s" || resp == "S")
+            {
+
+                Console.WriteLine("Digite o valor do deposito:");
+                double valor = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(nomeTitular, numeroConta, valor);
+                Console.WriteLine(conta);
+            }
+            else {
+
+                conta = new ContaBancaria(nomeTitular, numeroConta);
+                Console.WriteLine(conta);
+            }
+
+            conta.Deposito(500);
+
+           
         }
     }
 }
