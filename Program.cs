f8748f5e46123a9fe6f1c7e30e2a24bfad9b54be
IvesCostaBr <ivespauiniam@gmail.com>
2021-08-02 00:01:52 -0500

    exemple moduling

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConsoleApp;
+using Game.Lib;
+using Game.Interface;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador jogador1 = new Player("Ives Costa");
+            Jogador jogador2 = new Jogador2();
+            var game = new Game.Lib.Game.Game(jogador2);
+            game.StartGame();
+            var game2 = new Game.Lib.Game.Game(jogador1);
+            game2.StartGame();
+        }
+    }
+
+   
+}
+
